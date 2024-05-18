ff05af99464f2d29859c7757f75b26bd0d8bb921
kazibweyassin <kazibweusama@gmail.com>
2024-05-18 12:14:12 +0300

    added models amd end points

---
+++ server/src/models/review.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import modelOptions from "./model.options";
+
+const reviewSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    mediaType: {
+        type: String,
+        required: true,
+        enum: ["movie", "show"]
+    },
+    mediaId: {
+        type: String,
+        required: true
+    },
+    mediaTitle: {
+        type: String,
+        required: true
+    },
+    mediaPoster: {
+        type: String,
+        required: true
+    }
+}, modelOptions);
+
+export default mongoose.model("Review", reviewSchema);
